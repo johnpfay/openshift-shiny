c4e6c7c0250c0355554291def4aba3905c51a289
John Fay <john.fay@duke.edu>
2021-05-20 13:39:04 -0400

    Uses original SVI column names and data dictionary

--- examples/water-explorer/src/app.R
+++ examples/water-explorer/src/app.R
@@ -1,16 +1,35 @@
+### App to enable visualization of Water Usage vs Social Vulnerability
 
-
+#Load packages
 library(shiny)
 library(tidyverse)
 
-#import data
-the_data <- read_csv('water_data.csv') %>% 
+#Import data
+the_data <- read_csv('water_svi_data.csv') %>% 
     mutate(FIPS = as_factor(FIPS),
            year = as_factor(year)) 
 
+#Import & wrangle the data dictionary
+data_dict <- read_csv('SVIDocumentation_Table_DataDictionary_2018.csv') %>% 
+    select(`2018 VARIABLE NAME`, `2018 DESCRIPTION`) %>% 
+    filter(str_detect(`2018 VARIABLE NAME`, "\\bE")) %>% 
+    rename(name = `2018 VARIABLE NAME`,
+           desc = `2018 DESCRIPTION`)
+
 #Get field names
 water_fields <-  colnames(select(the_data,contains(".")))
-svi_fields <- colnames(select(the_data,starts_with("E")))
+svi_fields <- unlist(data_dict$name)
+svi_names <- unlist(data_dict$desc)
+
+#Function to get name from desc
+get_fldname <- function(the_desc){
+    fld_name <- data_dict %>% 
+        filter(desc == !!the_desc) %>%
+        select(name)
+    return (fld_name[[1]])
+    }
+
+#svi_fields <- colnames(select(the_data,starts_with("E_")))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -34,8 +53,8 @@ ui <- fluidPage(
             ),
             selectInput("DemField",
                         "Select SVI field",
-                        choices = svi_fields,
-                        selected = svi_fields[1])
+                        choices = svi_names,
+                        selected = svi_names[1])
             
         ),
 
@@ -48,15 +67,14 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    #Collect inputs
-    #the_year = input$YearSelect
-    x_col = "Household.TotalUse"
-    y_col = "E_POV"
     
+    #Create and show the scatterplot
     output$distPlot <- renderPlot({
+        #Translate SVI desc to name
+        sviField <- get_fldname(input$DemField)
         ggplot(filter(the_data ,year %in% input$YearSelect), 
                aes_string(x=input$UseField, 
-                          y=input$DemField)) +
+                          y=sviField)) +
             geom_point(aes(color=year)) +
             geom_smooth(method = "lm",aes(color=year))
     })
